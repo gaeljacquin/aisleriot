0829473dd9dc3399db08c072e376055e2485f05f
Gaël Jacquin <8724132+gaeljacquin@users.noreply.github.com>
2026-03-19 16:24:21 -0400

    build: pwa

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,8 +1,10 @@
+import { URL, fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -14,7 +16,51 @@ const config = defineConfig({
     tailwindcss(),
     tanstackRouter({ target: 'react', autoCodeSplitting: true }),
     viteReact(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'favicon.ico',
+        'logo.png',
+        'pwa-any-192.png',
+        'pwa-any-512.png',
+      ],
+      manifest: {
+        short_name: 'Aisleriot',
+        name: 'Aisleriot',
+        icons: [
+          {
+            src: '/favicon.ico',
+            sizes: '64x64 32x32 24x24 16x16',
+            type: 'image/x-icon',
+          },
+          {
+            src: '/pwa-any-192.png',
+            type: 'image/png',
+            sizes: '192x192',
+            purpose: 'any',
+          },
+          {
+            src: '/pwa-any-512.png',
+            type: 'image/png',
+            sizes: '512x512',
+            purpose: 'any',
+          },
+        ],
+        start_url: '.',
+        display: 'standalone',
+        theme_color: '#000000',
+        background_color: '#ffffff',
+      },
+    }),
   ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    allowedHosts: true,
+  },
 })
 
 export default config
